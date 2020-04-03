aa2f1c99dc25a6e0283960728237afb010a928a2
sibow <sibow@bloomington.in.gov>
2020-04-03 14:49:32 -0400

    added search and basic report to snap purchase module

--- src/bucks/ReportRow.java
+++ src/bucks/ReportRow.java
@@ -13,101 +13,101 @@ public class ReportRow implements java.io.Serializable{
 
     String row[]={"",""};
     boolean debug = false;
-		int size = 2;
-		static final long serialVersionUID = 90L;		
-		static Logger logger = LogManager.getLogger(ReportRow.class);
-		public ReportRow(boolean val){
-				debug = val;
-		}
+    int size = 2;
+    static final long serialVersionUID = 90L;		
+    static Logger logger = LogManager.getLogger(ReportRow.class);
+    public ReportRow(boolean val){
+	debug = val;
+    }
     public ReportRow(){
     }	
     public ReportRow(boolean val, int size){
-				debug = val;
-				setSize(size);
+	debug = val;
+	setSize(size);
     }
     public String getRowItem(int jj){
-				if(jj < 0 || jj >= size) return null;
-				return row[jj];
+	if(jj < 0 || jj >= size) return null;
+	return row[jj];
     }
-		public int getSize(){
-				return size;
-		}
+    public int getSize(){
+	return size;
+    }
     public String getFirst(){
-				return row[0];
+	return row[0];
     }
     public String getSecond(){
-				return row[1];
+	return row[1];
     }
     public String getThird(){
-				if(2 < size)
-						return row[2];
-				return "";
+	if(2 < size)
+	    return row[2];
+	return "";
     }
     public String getForth(){
-				if(3 < size)
-						return row[3];
-				return "";
+	if(3 < size)
+	    return row[3];
+	return "";
     }	
     //
     // setters
     //
-		/**
-		 * setting array size, can not be less than 2
-		 * @param size int value
-		 */
-		public void setSize(int size){
-				if(size > this.size){
-						row = new String[size];
-						for(int jj=0;jj<size;jj++){
-								row[jj] = "0"; // initialize;
-						}
-				}
-				this.size = size;
-		}
-    public void setRow(int jj, String val){
-				if(val != null && jj < size)
-						row[jj] = val;
+    /**
+     * setting array size, can not be less than 2
+     * @param size int value
+     */
+    public void setSize(int size){
+	if(size > this.size){
+	    row = new String[size];
+	    for(int jj=0;jj<size;jj++){
+		row[jj] = "0"; // initialize;
+	    }
+	}
+	this.size = size;
     }
-		/**
-		 * since many report will be two columns
-		 */
-		public void setRow(String val, String val2){
-				if(val != null){
-						row[0] = val;
-				}
-				if(val2 != null){
-						row[1] = val2;
-				}		
-		}
+    public void setRow(int jj, String val){
+	if(val != null && jj < size)
+	    row[jj] = val;
+    }
+    /**
+     * since many report will be two columns
+     */
+    public void setRow(String val, String val2){
+	if(val != null){
+	    row[0] = val;
+	}
+	if(val2 != null){
+	    row[1] = val2;
+	}		
+    }
 	
-		/**
-		 * we may have some report with three columns
-		 */
-		public void setRow(String val, String val2, String val3){
-				if(val != null){
-						row[0] = val;
-				}
-				if(val2 != null){
-						row[1] = val2;
-				}
-				if(val3 != null && size > 2){
-						row[2] = val3;
-				}		
-		}
-		//
-		public void setRow(String ... vals){
-				if(vals != null && vals.length == size){
-						row = vals;
-				}
-		}
-		//
+    /**
+     * we may have some report with three columns
+     */
+    public void setRow(String val, String val2, String val3){
+	if(val != null){
+	    row[0] = val;
+	}
+	if(val2 != null){
+	    row[1] = val2;
+	}
+	if(val3 != null && size > 2){
+	    row[2] = val3;
+	}		
+    }
+    //
+    public void setRow(String ... vals){
+	if(vals != null && vals.length == size){
+	    row = vals;
+	}
+    }
+    //
     public String toString(){
-				String ret = "";
-				for(int jj=0;jj<size;jj++){
-						if(!ret.equals("")) ret += ", ";
-						ret += row[jj];
-				}
-				return "["+ret+"]";
+	String ret = "";
+	for(int jj=0;jj<size;jj++){
+	    if(!ret.equals("")) ret += ", ";
+	    ret += row[jj];
+	}
+	return "["+ret+"]";
     }
 	
 }
